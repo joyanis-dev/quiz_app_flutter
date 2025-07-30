32621a200893776a33c0bcbdb8cf9751c6635988
Joy Anis <joyanis00@gmail.com>
2025-07-30 23:51:55 +0300

    Defined app routes in a clean and scalable way using AppRoutes class

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/constants/routes.dart';
+import 'package:quiz_app/views/login_view.dart';
+import 'package:quiz_app/views/quiz_view.dart';
+
+void main() {
+  runApp(const QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+     routes:{
+      AppRoutes.login: (context) => LoginView(),
+      AppRoutes.quiz : (context) => QuizView()
+     },
+     home:LoginView() ,
+    );
+  }
+}
